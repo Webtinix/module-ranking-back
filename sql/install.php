268fda75f432664c95a372f894789d22ca6c0d41
Orphée Lié <lieloumloum@gmail.com>
2024-05-13 15:32:49 +0200

    ok

--- sql/install.php
+++ sql/install.php
@@ -21,6 +21,64 @@ $sql = array (
         `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
         `date_upd` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
         PRIMARY KEY  (`id_ranking_product`)
+    ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;',  
+
+    'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . _DB_PREFIX_WEBTINIX_RANKING_ . 'groupe` (
+        `id_groupe` int(11) NOT NULL AUTO_INCREMENT,
+        `id_groupe_parent` int(11), 
+        `id_type_groupe` int(11), 
+        `level` int(11),
+        `note` int(11),
+        `active` int(11) NOT NULL DEFAULT 1,
+        `position` int(11),
+        `date_begin` timestamp NOT NULL,
+        `date_end` timestamp NOT NULL,
+        `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `date_upd` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY  (`id_groupe`)
+    ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;',    
+
+    'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . _DB_PREFIX_WEBTINIX_RANKING_ . 'type_groupe` (
+        `id_type_groupe` int(11) NOT NULL AUTO_INCREMENT,
+        `code` text NOT NULL,
+        `position` int(11),
+        PRIMARY KEY  (`id_type_groupe`)
+    ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;',  
+    
+    "DELETE FROM `" . _DB_PREFIX_ . _DB_PREFIX_WEBTINIX_RANKING_ . "type_groupe`;
+    INSERT INTO `" . _DB_PREFIX_ . _DB_PREFIX_WEBTINIX_RANKING_ . "type_groupe` (`id_type_groupe`, `code`, `position`) VALUES
+    (1, '12_last_months', 0),
+    (2, 'promo', 1),
+    (3, 'id_product', 2),
+    (4, 'popularity', 3),
+    (5, 'custom_filter_product_criteria', 4);",
+
+    'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . _DB_PREFIX_WEBTINIX_RANKING_ . 'operator_groupe` (
+        `id_operator_groupe` int(11) NOT NULL AUTO_INCREMENT,
+        `id_groupe_left` int(11), 
+        `id_groupe_right` int(11),
+        `operator` varchar(255) NOT NULL,
+        PRIMARY KEY  (`id_operator_groupe`)
+    ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;',    
+
+    'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . _DB_PREFIX_WEBTINIX_RANKING_ . 'operator_criteria` (
+        `id_operator_criteria` int(11) NOT NULL AUTO_INCREMENT,
+        `id_criteria_left` int(11), 
+        `id_criteria_right` int(11),
+        `operator` varchar(255) NOT NULL,
+        PRIMARY KEY  (`id_operator_criteria`)
+    ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;',  
+
+    'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . _DB_PREFIX_WEBTINIX_RANKING_ . 'criteria` (
+        `id_criteria` int(11) NOT NULL AUTO_INCREMENT,
+        `criteria_product` int(11), 
+        `comparison_operator` int(11), 
+        `values` text NOT NULL,
+        `active` int(11) NOT NULL DEFAULT 1,
+        `position` int(11),
+        `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `date_upd` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY  (`id_criteria`)
     ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;',    
 );
     
--- wx_ranking.php
+++ wx_ranking.php
@@ -20,6 +20,7 @@ class Wx_Ranking extends Module implements WidgetInterface
     private $default_position_ranking     = 0;
     private $default_category_ranking     = 0;
     private $default_id_product_attribute = 0;
+    private $admin_url = '';
 
     public function __construct()
     {
@@ -30,6 +31,11 @@ class Wx_Ranking extends Module implements WidgetInterface
         $this->need_instance = 0;
         $this->display_warnings = null;
         $this->display_messages = null;
+        $this->admin_url = AdminController::$currentIndex.
+        '&token='.Tools::getAdminTokenLite('AdminModules').
+        '&configure='.$this->name.
+        '&tab_module='.$this->tab.
+        '&module_name='.$this->name;
 
         /**
          * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
@@ -82,7 +88,9 @@ class Wx_Ranking extends Module implements WidgetInterface
         Configuration::updateValue('WX_RANKING_DEFAULT_CATEGORY_RANKING',     $this->default_category_ranking);
         Configuration::updateValue('WX_RANKING_DEFAULT_ID_PRODUCT_ATTRIBUTE', $this->default_id_product_attribute);
 
-        return parent::install();
+        return parent::install() &&
+            $this->registerHook('wxHandlerListingAdminMenu') &&
+            $this->registerHook('wxHandlerListingAdminMenuAddOptionRankingAfter');
             // &&
             // $this->registerHook('header') &&
             // $this->registerHook('backOfficeHeader') &&
@@ -106,47 +114,334 @@ class Wx_Ranking extends Module implements WidgetInterface
      */
     public function getContent()
     {
+        $output = '';
+
+        // get controller
+        $controller = Tools::getValue('moduleController');
+
+        // baseAdminModuleUrl
+        $baseAdminModuleUrl = AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules').'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+
+        // display any message, then the form
+        if ($controller=='carrierConfiguration'){
+
+            // if (Tools::getIsset('deletecustom_field') && Tools::getValue('id_custom_field')!=''){
+                
+            // } else {
+            //     $output = $this->FieldForm();
+            // }
+
+            $carrier_admin = new CarrierAdministration($this);
+            $output = $carrier_admin->renderView();
+
+        } else if ($controller=='getCustomFields'){
+            // get customer custom fields
+            $output = $this->LocationFieldsList(Tools::getValue('location'));
+        }  
+        else if ($controller=='bannerProgConfiguration'){
+            // get customer custom fields
+            $banner_admin = new BannerProgAdministration($this);
+            $output = $banner_admin->renderView();
+        }   
+        else if ($controller=='bannerAnyWhereConfiguration'){
+            // get customer custom fields
+            $banner_admin = new BannerAnyWhereAdministration($this);
+            $output = $banner_admin->renderView();
+        } 
+        else if ($controller=='seeInfosCustomerOrderConfiguration' && Tools::getValue('id_customer')){
+            // get customer custom fields
+            $banner_admin = new InfosCustomerOrderAdministration($this);
+            $banner_admin->setCustomer(Tools::getValue('id_customer'));
+            $output = $banner_admin->renderView();
+        } 
+        else {
+            // get module dashboard page
+            $output = $this->Dashboard();
+        }
+
         
+        // gestion des actions
+        if (Tools::getValue('moduleAction')) {
+            # code...
+            $action = Tools::getValue('moduleAction');
+
+            if ($action == 'loginAsCustomer' && Tools::getValue('id_customer')) {
+                # code...
+                $id_customer = Tools::getValue('id_customer');
+                // $customer = new Customer($id_customer);
+                // if (!Validate::isLoadedObject($cart)) {
+                //     $this->context->updateCustomer($customer);
+                // }
+                $uuid = uniqid();
+                // die(Tools::getValue('id_customer'));
+                \DB::getInstance()->insert(_DB_PREFIX_WEBTINIX_ . 'login_as_custom', [
+                    'token' => Tools::getAdminTokenLite('AdminModules'),
+                    'id_customer' => $id_customer,
+                    'id_employee' => $context->employee->id,
+                    'uuid' => $uuid,
+                ]);
+
+                // echo '<pre>';
+                // var_dump($uuid);
+                // echo '</pre>';
+                // die();
+                $my_account = $this->context->link->getPageLink('my-account', true);
+                Tools::redirect($my_account . '?moduleAction=loginAsCustomer&uuid=' . $uuid);
+            }
+            // elseif ($action == 'loginAsCustomer' && Tools::getValue('uuid')) {
+            //     # code...
+            //     $uuid = Tools::getValue('uuid');
+            //     $uuid = htmlspecialchars($uuid);
+
+            //     $id_customers = \Db::getInstance()->executeS(
+            //         "SELECT id_webservice FROM `" . _DB_PREFIX_WEBTINIX_ . "login_as_custom` WHERE `uuid` = $uuid ",
+            //         $array = true, $use_cache = false
+            //     );
+
+            //     echo '<pre>';
+            //     var_dump($id_customers);
+            //     echo '</pre>';
+            //     die();
+
+            //     if (!empty($id_customers)) {
+            //         # code...
+            //         $id_customer = Tools::getValue('id_customer');
+            //         $customer = new Customer($id_customer);
+            //         if (!Validate::isLoadedObject($cart)) {
+            //             $this->context->updateCustomer($customer);
+            //         }
+            //         $my_account = $this->context->link->getPageLink('my-account', true);
+            //         Tools::redirect($my_account);
+            //     }else{
+            //         Tools::redirect('/');
+            //     }
+            // }
+        }
+        
+        // return
+        return $this->getHeader().$output.$this->getFooter();
+    }
+    
+    /*
+    * Dashboard
+    */
+    public function Dashboard()
+    {
+        // assign to smarty
+        $this->smarty->assign(array(
+                                    'uri' => $this->admin_url
+                                    ));
+        // return template
+        return $this->display(__FILE__, '/views/templates/admin/dashboard.tpl');
+    }
+
+    /*
+    * getHeader in admin
+    */
+    public function getHeader()
+    {
+        // get controller
+        // $controller = Tools::getValue('moduleController');
+        // // check if dashboard
+        // if ($controller==''){
+        //     $dashboard = true;
+        // } else {
+        //     $dashboard = false;
+        // }
+        // show current store only if multistore is enabled
+        if (Configuration::get('PS_MULTISHOP_FEATURE_ACTIVE')) {
+            $displayCurrentStore = true;
+        } else {
+            $displayCurrentStore = false;
+        }
+
+        // Menu des modules Ranking, Recommandations, et les autres modules qui seront liés au listing
+        $menu = [];
+        $menu = Hook::exec('wxHandlerListingAdminMenu', ['menu' => $menu], 
+            $moduleId = null, 
+            $returnArray = false, 
+            $checkExceptions = true, 
+            $usePush = false, 
+            $shopId = null, 
+            $chain = true
+        );
+
+        // assign to smarty
+        $this->smarty->assign(array(
+                                    'shop'                => $this->context->shop->id,
+                                    'storeName'           => $this->context->shop->name ,
+                                    'logoSrc'             => _PS_BASE_URL_.$this->_path.'/views/img/logo.png',
+                                    'moduleImageDir'      => _PS_BASE_URL_.$this->_path.'/views/img/',
+                                    'psVersion'           => $this->psVersion(),
+                                    'displayCurrentStore' => $displayCurrentStore,
+                                    'menu'                => $menu,
+                                    'moduleVersion'       => $this->version,
+                                    // 'dashboard'           => $dashboard,
+                                    // 'controller'          => $controller,
+                                    // 'uri'                 => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules').'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name.''
+                                    ));
+        // return
+        return $this->display(__FILE__, '/views/templates/admin/header.tpl');
+    }
+
+    /*
+    * getFooter in Admin
+    */
+    public function getFooter()
+    {
+        $this->smarty->assign(array(
+                                    'moduleVersion'       => $this->version,
+                                    ));
+        return $this->display(__FILE__, '/views/templates/admin/footer.tpl');
+    }
+
+    // On rajoute des éléments au header
+    public function hookWxHandlerListingAdminMenu(array $params)
+    {
+        // Le tableau de bord vient en premier
+        $params['menu'][] = $this->getOptionMenu($option = 'dashboard');
+
+        // On peut rajouter une nouvelle option avant le ranking
+        $option = [];
+        $option = Hook::exec('wxHandlerListingAdminMenuAddOptionRankingBefore', 
+            ['option' => $option], 
+            $moduleId = null, 
+            $returnArray = false, 
+            $checkExceptions = true, 
+            $usePush = false, 
+            $shopId = null, 
+            $chain = true
+        );
+
+        if (!empty($option) && !empty($option['option'])) {
+            # code...
+            $params['menu'][] = $option['option'];
+        }
+
+        // On ajoute le Ranking
+        $params['menu'][] = $this->getOptionMenu($option = 'ranking');
+        
+        // On peut rajouter une nouvelle option après le ranking
+        $option = [];
+        $option = Hook::exec('wxHandlerListingAdminMenuAddOptionRankingAfter', 
+            ['option' => $option], 
+            $moduleId = null, 
+            $returnArray = false, 
+            $checkExceptions = true, 
+            $usePush = false, 
+            $shopId = null, 
+            $chain = true
+        );
+
+        if (!empty($option) && !empty($option['option'])) {
+            # code...
+            $params['menu'][] = $option['option'];
+        }
+
+        // L'aide vient en dernier
+        $params['menu'][] = $this->getOptionMenu($option = 'help');
+
+        return $params;
+    }
+
+
+    public function getOptionMenu($option = null) {
+        
+        $title = '';
+        $url = $this->admin_url;
+
+        switch ($option) {
+            case 'dashboard':
+                # code...
+                $title = $this->l('Tableau de bord');
+                $url .= '';
+                break;
+            case 'ranking':
+                # code...
+                $title = $this->l('Ranking');
+                $url .= '';
+                break;
+            case 'help':
+                # code...
+                $title = $this->l('Aide');
+                $url .= '';
+                break;
+            case 'reco':
+                # code...
+                $title = $this->l('Recommandation');
+                $url .= '';
+                break;
+            
+            default:
+                # code...
+                $title = $this->l('...');
+                break;
+        }
+
+        $option_menu = array(
+            'title' => $title,
+            'url'   => $url,
+        ); 
+
+        return $option_menu;
+    }
+
+    public function hookWxHandlerListingAdminMenuAddOptionRankingAfter(array $params) {
+        
+        // Le tableau de bord vient en premier
+        $params['option'][] = $this->getOptionMenu($option = 'reco');
+
+        return $params;
+    }
+
+    /*
+    * psVersion
+    */
+    public static function psVersion()
+    {
+        $version = _PS_VERSION_;
+        $exp = explode('.', $version);
+        return $exp[0].'.'.$exp[1];
     }
 
     /**
     * Add the CSS & JavaScript files you want to be loaded in the BO.
     */
-    // public function hookBackOfficeHeader()
-    // {
+    public function hookBackOfficeHeader()
+    {
         
-    // }
+    }
 
-    // /**
-    //  * Add the CSS & JavaScript files you want to be added on the FO.
-    //  */
-    // public function hookHeader()
-    // {
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
         
-    // }
+    }
 
-    // /* 1.7 */
-    // public function hookDisplayWxProductButtons()
-    // {
+    /* 1.7 */
+    public function hookDisplayWxProductButtons()
+    {
         
         
-    // }
+    }
 
-    // public function hookDisplayRecoFPMobile()
-    // {
+    public function hookDisplayRecoFPMobile()
+    {
         
-    // }
+    }
 
 
-    // public function hookDisplayCartModalFooter()
-    // {
+    public function hookDisplayCartModalFooter()
+    {
         
-    // }
+    }
 
-    // public function hookDisplayShoppingCartFooter()
-    // {
+    public function hookDisplayShoppingCartFooter()
+    {
         
-    // }
+    }
 
     public function renderWidget($hookName = null, array $configuration = [],$params = [])
     {   
@@ -166,8 +461,8 @@ class Wx_Ranking extends Module implements WidgetInterface
 
     public function getWidgetVariables($hookName = null, array $configuration = [], array $params=[])
     {
-        // echo '<pre>'; var_dump($hookName); echo '</pre>';
-        // die();
+        echo '<pre>'; var_dump($hookName); echo '</pre>';
+        die();
         if (isset($hookName) && in_array($hookName, ['displayRecoFPMobile','displayHome', 'displayWxProductButtons', 'displayModalCartCrosseling', 'displayShoppingCartFooter','displayBrandPage', 'displayCategoryPage'])) {
             if ($hookName == 'displayWxProductButtons' OR $hookName == 'displayRecoFPMobile' ) {
                 $configHookName = 'displayFooterProduct';
